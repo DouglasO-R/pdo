617769c8982ab24140a56b00f560b71517896092
Douglas O.R <douglasor@outlook.com>
2017-06-19 21:21:37 -0300

    pdo tratando sql injection

--- index.php
+++ index.php
@@ -3,17 +3,13 @@
 try{
 
 $conn = new \PDO("mysql:host=localhost;dbname=test","root","");
-$query = "select * from produtos where id = {$_GET['id']}";
-//$query = "insert into produtos(nome,descricao) values('caneta','caneta nova')";
-foreach ($conn->query($query) as $produto) {
-	echo $produto['id'];
-	echo "<br>";
-	echo $produto['nome'];
-	echo "<br>";
-	echo $produto['descricao'];
-	echo "<br>";
-}
 
+$query = "select * from produtos where id =:id";
+$stmt = $conn->prepare($query);
+$stmt->bindValue(':id',$_GET['id']);
+$stmt->execute();
+
+print_r($stmt->fetchALL());
 
 }catch(\PDOException $e){
 	echo "Error! Message: ".$e->getMessage()." Code: ".$e->getCode();
