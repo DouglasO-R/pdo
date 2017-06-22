2799bd73a96414ab7187d07be2528379b70cdeb5
Douglas O.R <douglasor@outlook.com>
2017-06-21 22:02:29 -0300

    preparando crud com service produto

---
+++ IProduct.php
@@ -0,0 +1,11 @@
+<?php
+
+interface IProduct{
+
+	public function getId();
+    public function setId($id);
+    public function getNome();
+    public function setNome($nome);
+    public function getDescricao();
+    public function setDescricao($descricao);
+}
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
 require_once"Conn.php";
 require_once"IConn.php";
+require_once"IProduct.php";
+require_once"Product.php";
