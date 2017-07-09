b54cd6d0945c1268241bf0cd545edffbb020ddd1
Douglas O.R <douglasor@outlook.com>
2017-07-09 19:13:55 -0300

    mudando confing

--- public/config.php
+++ public/config.php
@@ -5,7 +5,7 @@ use Pimple\Container;
 
 $container = new Container;
 
-$container['host'] => $_ENV["DATABASE_TESTHOST"];
-$container['dbname'] => $_ENV["DATABASE_TESTDBNAME"];
-$container['user'] => $_ENV["DATABASE_TESTDBUSER"];
-$container['pass'] => $_ENV["DATABASE_TESTDBPASS"];
+$container['host'] = $_ENV["DATABASE_TESTHOST"];
+$container['dbname'] = $_ENV["DATABASE_TESTDBNAME"];
+$container['user'] = $_ENV["DATABASE_TESTDBUSER"];
+$container['pass'] = $_ENV["DATABASE_TESTDBPASS"];
